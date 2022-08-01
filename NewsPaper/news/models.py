728f88a62742d7eaafefc07b721ba3670d70ce63
Illmix <46610176+Illmix@users.noreply.github.com>
2022-08-02 00:14:20 +0200

    Add files via upload

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -1,19 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Sum
 
 
 class Author(models.Model):
-    user_rating = models.IntegerField(default=0)
+    user_rating = models.SmallIntegerField(default=0)
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
-    def update_rating(self, rate):
-        rate += self.user.post_rating*3
-        rate += self.user.comment_rating
-        return rate
+    def update_rating(self):
+        post_r = self.post_set.all().aggregate(postRating=Sum('post_rating'))
+        p_rat = 0
+        p_rat += post_r.get('postRating')
+        comment_r = self.user.comment_set.all().aggregate(commentRating=Sum('comment_rating'))
+        c_rat = 0
+        c_rat += comment_r.get('commentRating')
+        self.user_rating = p_rat * 3 + c_rat
+        self.save()
 
 
 class Category(models.Model):
-    category_name = models.CharField(max_length=255, unique=True)
+    category_name = models.CharField(max_length=100, unique=True)
 
 
 class Post(models.Model):
@@ -23,14 +29,14 @@ class Post(models.Model):
         (news, 'News'),
         (article, 'Article')
     ]
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, default=news)
     n_or_a = models.CharField(max_length=2, choices=NEWS_OR_ARTICLE)
     create_date = models.DateTimeField(auto_now_add=True)
     create_time = models.TimeField(auto_now_add=True)
     category = models.ManyToManyField(Category, through='PostCategory')
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=120)
     body = models.TextField()
-    post_rating = models.IntegerField(default=0)
+    post_rating = models.SmallIntegerField(default=0)
 
     def like(self):
         self.post_rating += 1
@@ -42,7 +48,10 @@ class Post(models.Model):
         self.save()
 
     def preview(self):
-        return self.body[:124]+'....'
+        if len(self.body) >= 124:
+            return self.body[:124]+'....'
+        else:
+            return self.body
 
 
 class PostCategory(models.Model):
@@ -56,7 +65,7 @@ class Comment(models.Model):
     text = models.CharField(max_length=500, default='Comment: ')
     create_date = models.DateTimeField(auto_now_add=True)
     create_time = models.TimeField(auto_now_add=True)
-    comment_rating = models.IntegerField(default=0)
+    comment_rating = models.SmallIntegerField(default=0)
 
     def like(self):
         self.comment_rating += 1
@@ -67,5 +76,5 @@ class Comment(models.Model):
             self.comment_rating -= 1
         self.save()
 
-    def get_username(self):
+    def __str__(self):
         return self.user.username
